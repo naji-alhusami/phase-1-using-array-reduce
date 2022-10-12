9560547397867a74cd0973c75caad632a4bdb3bf
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-12 17:49:59 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce(function (total, amount) {
+  return total + amount;
+});
+
+console.log(totalBatteries);
+
+/* const sum = batteryBatches.reduce( function(total, amount){
+    return total + amount;
+  });
+  console.log(sum); */
+
+/* totalBatteries.push(
+  batteryBatches.reduce(function (acc, currVal) {
+    acc + currVal;
+  })
+);
+console.log(totalBatteries);
+ */
